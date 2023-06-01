5c1bff53b838d5de678053c2b73089fc2db01340
LilboX3 <lilligruber02@gmail.com>
2023-06-01 17:01:27 +0200

    memory bissl was

---
+++ Memory/memory.js
@@ -0,0 +1,159 @@
+
+
+var seconds = 0;
+var timer = document.getElementById('time');
+const images = [];
+images.length = 16;
+var pick = 0;
+var tries = 0;
+var toCompare;
+var clicked = 0;
+var score = 0;
+
+fillArray(images);
+
+
+function incrementSeconds(cancel) {
+    if(score>=16){
+        clearInterval(cancel);
+        timer.innerHTML = "Gewonnen in: " + seconds + " sek.";
+        return;
+    }
+    seconds += 1;
+    timer.innerHTML = "Zeit: " + seconds + " sek.";
+}
+
+function timeDisplay(){
+    var cancel = setInterval(incrementSeconds, 1000, cancel);
+}
+
+function fillArray(){
+    for(let i=0;i<images.length;i++){
+        if(i>=8){
+            images[i]=document.createElement("img")
+            images[i].setAttribute("src", "Memory/images/unaufgedeckt.png");
+            images[i].setAttribute("class", "img"+(i-8));
+
+
+        } else{
+        images[i]= document.createElement("img")
+        images[i].setAttribute("src", "Memory/images/unaufgedeckt.png");
+        images[i].setAttribute("class", "img"+i);
+    
+        }
+        images[i].setAttribute("onClick", "showPic(this)");
+        images[i].setAttribute("id", "pos"+i);
+    }
+    shuffleArray();
+}
+
+function shuffleArray(){
+    for(let i=0;i<images.length;i++){
+        let pos = Math.floor(Math.random()*16);
+        let temp = images[pos];
+        images[pos] = images[i];
+        images[i] = temp;
+    }
+    createHTML();
+}
+
+function createHTML(){
+    const spielbereich = document.querySelector("#spielbereich");
+
+    for(let i=0;i<images.length;i++){
+        spielbereich.appendChild(images[i]);
+        if((i+1)%4==0){
+           spielbereich.appendChild(document.createElement("BR"));
+        }
+    
+    }
+
+}
+
+function found(img){
+    document.getElementById(toCompare.id).setAttribute("src", "Memory/images/aufgedeckt.png");
+    document.getElementById(img.id).setAttribute("src", "Memory/images/aufgedeckt.png");
+    document.getElementById(toCompare.id).removeAttribute("onClick");
+    document.getElementById(img.id).removeAttribute("onClick");
+    toCompare = null;
+    clicked = 0;
+    score += 2;
+}
+
+function hide(img){
+    document.getElementById(toCompare.id).setAttribute("src", "pics/memoryBg.png");
+        document.getElementById(img.id).setAttribute("src", "pics/memoryBg.png");
+        toCompare = null;
+        clicked = 0;
+}
+
+function comparePics(img){
+    if(toCompare.className ==img.className &&toCompare.id!=img.id){
+        const myTimeout = setTimeout(found, 2000, img);
+        //clearTimeout(myTimeout);
+    } else {
+        const myTimeout2 = setTimeout(hide, 2000, img);
+        //clearTimeout(myTimeout2);
+    }
+    
+    
+}
+
+function showPic(img){
+    if(clicked>=2){
+        return;
+    }
+    clicked++;
+    let elClass = img.className;
+    let id = img.id;
+    let element = document.getElementById(id);
+    if(pick<3){
+        pick++;
+    }
+    if(pick>=2){
+        pick = 0;
+        countTry();
+    }
+
+    if(elClass == "img0"){
+        element.setAttribute("src","images/card1.png");
+    }
+    else if(elClass == "img1"){
+        element.setAttribute("src","images/card2.png");
+    }
+    else if(elClass == "img2"){
+        element.setAttribute("src","images/card3.png");
+    }
+    else if(elClass == "img3"){
+        element.setAttribute("src","images/card4.png");
+    }
+    else if(elClass == "img4"){
+        element.setAttribute("src","images/card5.png");
+    }
+    else if(elClass == "img5"){
+        element.setAttribute("src","images/card6.png");
+    }
+    else if(elClass == "img6"){
+        element.setAttribute("src","images/card7.png");
+    }
+    else if(elClass == "img7"){
+        element.setAttribute("src","images/card8.png");
+    }
+
+    if(toCompare==null){
+        toCompare = img;
+    } else {
+        comparePics(img);
+    }
+
+
+}
+
+function countTry(){
+    tries++;
+    document.getElementById("tries").innerHTML = "Versuche: "+tries;
+}
+
+
+
+
---
+++ memory.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<?php
+include_once 'navbar.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="Memory/memory.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <title>Play Memory!</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+  <div class="col-1"> 
+    
+    </div>
+  <div class="col-10">
+ <!--<div class="infobox">
+    <div class="labels">
+      Spieler:  
+    </div>
+    <div id="showname" class="labels">
+      <form name="name" id="yourname" onSubmit="return setName()">
+      <input type="text"> <input type="submit" value="done">
+    </form>
+    </div>
+ </div>-->
+ 
+ 
+ <div id = "spielbereich">
+    
+  </div>
+  
+  <div class="infobox">
+  <div id="tries" class="labels">
+      Tries: 0
+  </div>
+  <div class="labels">
+      
+  </div>
+ </div>
+   <div class="infobox">
+   <div class="labels" id="time">
+      Time passed:
+      <button onClick="timeDisplay()" id="timebutton">Start a timer!</button>
+   </div>
+   <div class="labels">
+  
+     </div>
+   </div>
+  </div>
+
+   <div class="col-1"> 
+    
+  </div>
+ </div>
+
+
+</div>
+<script src="Memory/memory.js"></script>
+</body>
+</html>
