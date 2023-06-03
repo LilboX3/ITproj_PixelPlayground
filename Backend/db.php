05cae49b11f5f96f818a4ab7bbc8a50384731524
Toukage <127383796+Toukage@users.noreply.github.com>
2023-06-04 00:00:25 +0200

    Update db.php

--- Backend/db.php
+++ Backend/db.php
@@ -1,29 +1,58 @@
 <?php
-$servername = "localhost"; // Or enter your server name here
-$username = "bif2webscriptinguser"; // Enter your MySQL username here
-$password = "bif2021"; // Enter your MySQL password here
-$dbname = "PixelPlayground"; // Enter your database name here
+$servername = "localhost";
+$username = "bif2webscriptinguser";
+$password = "bif2021";
+$dbname = "PixelPlayground";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$table = "users";
+$username = isset($_POST['username']) ? $_POST['username'] : '';
+$score = isset($_POST['score']) ? $_POST['score'] : '';
+$gameName = isset($_POST['game']) ? $_POST['game'] : '';
 
-$columns = array("username", "password");
+if (!empty($username)) {
+    // User is logged in or entered a username
+    $userExists = false;
+    
+    // Check if the username already exists
+    $checkUsernameQuery = "SELECT id FROM users WHERE username = '$username'";
+    $result = $conn->query($checkUsernameQuery);
+    
+    if ($result->num_rows > 0) {
+        // Username already exists, use the existing user
+        $userExists = true;
+        $row = $result->fetch_assoc();
+        $userId = $row['id'];
+    }
+    
+    if (!$userExists) {
+        // Create a new user
+        $createUserQuery = "INSERT INTO users (username) VALUES ('$username')";
+        if ($conn->query($createUserQuery) === TRUE) {
+            $userId = $conn->insert_id;
+        } else {
+            echo "Error creating new user: " . $conn->error;
+            exit();
+        }
+    }
+    
+    $sql = "INSERT INTO highscore (game_id, user_id, score) 
+            VALUES (
+                (SELECT id FROM game WHERE name = '$gameName'),
+                '$userId',
+                $score
+            )";
+} 
 
-$query = "SELECT " . implode(",", $columns) . " FROM " . $table;
-
-$result = mysqli_query($conn, $query);
-
-if (!$result) {
-    die("Query failed: " . mysqli_error($conn));
+if ($conn->query($sql) === TRUE) {
+    echo "Score saved successfully.";
+} else {
+    echo "Error: " . $conn->error;
 }
 
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "username: " . $row["username"] . "password: " . $row["password"] . "<br>";
-}
+$conn->close();
+?>
