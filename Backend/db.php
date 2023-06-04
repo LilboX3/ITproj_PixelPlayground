4b38b059e81ba07ff1be59ce3271e3e51d9bda47
Toukage <127383796+Toukage@users.noreply.github.com>
2023-06-04 03:59:26 +0200

    Update db.php

--- Backend/db.php
+++ Backend/db.php
@@ -16,7 +16,6 @@ $gameName = isset($_POST['game']) ? $_POST['game'] : '';
 
 if (!empty($username)) {
     // User is logged in or entered a username
-    $userExists = false;
     
     // Check if the username already exists
     $checkUsernameQuery = "SELECT id FROM users WHERE username = '$username'";
@@ -24,12 +23,9 @@ if (!empty($username)) {
     
     if ($result->num_rows > 0) {
         // Username already exists, use the existing user
-        $userExists = true;
         $row = $result->fetch_assoc();
         $userId = $row['id'];
-    }
-    
-    if (!$userExists) {
+    } else {
         // Create a new user
         $createUserQuery = "INSERT INTO users (username) VALUES ('$username')";
         if ($conn->query($createUserQuery) === TRUE) {
@@ -40,18 +36,40 @@ if (!empty($username)) {
         }
     }
     
-    $sql = "INSERT INTO highscore (game_id, user_id, score) 
-            VALUES (
-                (SELECT id FROM game WHERE name = '$gameName'),
-                '$userId',
-                $score
-            )";
-} 
+    // Check if the user already has a score for the given game
+    $checkScoreQuery = "SELECT score FROM highscore WHERE user_id = '$userId' AND game_id = (SELECT id FROM game WHERE name = '$gameName')";
+    $result = $conn->query($checkScoreQuery);
+    
+    if ($result->num_rows > 0) {
+        // User has a previous score for the game
+        $row = $result->fetch_assoc();
+        $previousScore = $row['score'];
+        
+        if ($score <= $previousScore) {
+            // New score is not higher, do not save it
+            echo "Score not saved. User already has a higher score for this game.";
+            exit();
+        } else {
+            // New score is higher, update the existing score
+            $updateScoreQuery = "UPDATE highscore SET score = '$score' WHERE user_id = '$userId' AND game_id = (SELECT id FROM game WHERE name = '$gameName')";
+            if ($conn->query($updateScoreQuery) === FALSE) {
+                echo "Error updating score: " . $conn->error;
+                exit();
+            }
+        }
+    } else {
+        // User does not have a previous score for the game, insert a new score
+        $insertScoreQuery = "INSERT INTO highscore (game_id, user_id, score) 
+                             VALUES ((SELECT id FROM game WHERE name = '$gameName'), '$userId', '$score')";
+        if ($conn->query($insertScoreQuery) === FALSE) {
+            echo "Error inserting score: " . $conn->error;
+            exit();
+        }
+    }
 
-if ($conn->query($sql) === TRUE) {
     echo "Score saved successfully.";
 } else {
-    echo "Error: " . $conn->error;
+    echo "Username is required.";
 }
 
 $conn->close();
