172e2ee8d0a2bae003ac547282cf6d05af94f19f
Toukage <127383796+Toukage@users.noreply.github.com>
2023-06-05 08:44:19 +0200

    Update Tetris.php

--- Tetris.php
+++ Tetris.php
@@ -3,8 +3,9 @@ include_once 'navbar.php';
 ?>
 
     <title>Tetris</title>
-    <link rel="stylesheet" href="Tetris/tetris.css">
+    <link rel="stylesheet" href="Tetris/style.css">
     <link rel="stylesheet" href="Tetris/prstartk.ttf">
+    <<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="background">
     <div class="container">
