c507b319cc7c37c976768c8e5e5e261a6b5d1a24
LilboX3 <lilligruber02@gmail.com>
2023-06-07 18:12:43 +0200

    homepage updated

--- Homepage.php
+++ Homepage.php
@@ -19,21 +19,45 @@ include_once 'navbar.php';
 
         <!-- Full-width images with number and caption text -->
         <div class="mySlides fade2">
-          <div class="numbertext">1 / 3</div>
-          <img src="pics/pacman.png" alt="game 1" style="width:100%;height:400px;">
-          <div class="text">Play Pac Man</div>
+          <div class="numbertext">1 / 7</div>
+          <img src="Homepage/imgs/Hangman.png" alt="game 1" style="width:100%;" onclick="window.open('Hangman.php')">
+          <div class="text">Play Hangman</div>
         </div>
 
         <div class="mySlides fade2">
-          <div class="numbertext">2 / 3</div>
-          <img src="pics/pong.png" alt="game 2" style="width:100%;height:400px;">
+          <div class="numbertext">2 / 7</div>
+          <img src="Homepage/imgs/Pong.png" alt="game 2" style="width:100%; " onclick="window.open('Pong.php')">
           <div class="text">Play Pong</div>
         </div>
 
         <div class="mySlides fade2">
-          <div class="numbertext">3 / 3</div>
-          <img src="pics/snake.png" alt="game 3" style="width:100%;height:400px;">
+          <div class="numbertext">3 / 7</div>
+          <img src="Homepage/imgs/Snake.png" alt="game 3" style="width:100%; " onclick="window.open('Snake.php')">
           <div class="text">Play Snake</div>
+        </div>
+
+        <div class="mySlides fade2">
+          <div class="numbertext">4 / 7</div>
+          <img src="Homepage/imgs/Memory.png" alt="game 4" style="width:100%; " onclick="window.open('memory.php')">
+          <div class="text">Play Memory</div>
+        </div>
+
+        <div class="mySlides fade2">
+          <div class="numbertext">5 / 7</div>
+          <img src="Homepage/imgs/Tetris.png" alt="game 5" style="width:100%; " onclick="window.open('Tetris.php')">
+          <div class="text">Play Tetris</div>
+        </div>
+
+        <div class="mySlides fade2">
+          <div class="numbertext">6 / 7</div>
+          <img src="Homepage/imgs/Tictactoe.png" alt="game 6" style="width:100%; " onclick="window.open('TicTacToe.php')">
+          <div class="text">Play TicTacToe</div>
+        </div>
+
+        <div class="mySlides fade2">
+          <div class="numbertext">7 / 7</div>
+          <img src="Homepage/imgs/Scramble.png" alt="game 7" style="width:100%;" onclick="window.open('WordScramble.php')">
+          <div class="text">Play Word Scramble</div>
         </div>
 
         <!-- Next and previous buttons -->
@@ -54,6 +78,6 @@ include_once 'navbar.php';
   </div>
   
 </div>
-<script src="slideshow.js"></script>
+<script src="Homepage/slideshow.js"></script>
 </body>
 </html>
