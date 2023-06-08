180ed8db4e8bb2d266793eb7a842d88e12139424
mehdier0 <119071443+mehdier0@users.noreply.github.com>
2023-06-08 19:58:34 +0200

    Add files via upload

---
+++ WordScramble/data.js
@@ -0,0 +1,45 @@
+const levels = [
+    {
+        name: "Level 1",
+        words: [
+            { word: "Haus", hint: "Ein Ort, an dem Menschen leben" },
+            { word: "Baum", hint: "Ein großer Pflanze mit Ästen und Blättern" },
+            { word: "Auto", hint: "Ein Fahrzeug mit vier Rädern" },
+            { word: "Buch", hint: "Enthält Informationen oder Geschichten" },
+            { word: "Stuhl", hint: "Man kann darauf sitzen" },
+            { word: "Maus", hint: "Ein kleines Tier mit einem langen Schwanz" },
+            { word: "Sonne", hint: "Ein Stern, der Licht und Wärme gibt" },
+            { word: "Tisch", hint: "Man kann Dinge darauf ablegen" }
+        ],
+    },
+    {
+        name: "Level 2",
+        words: [
+            { word: "Gitarre", hint: "Ein Musikinstrument mit Saiten" },
+            { word: "Blume", hint: "Eine Pflanze, die oft schön riecht" },
+            { word: "Fluss", hint: "Ein langer, fließender Wasserweg" },
+            { word: "Kamera", hint: "Wird verwendet, um Fotos zu machen" },
+            { word: "Laptop", hint: "Ein tragbarer Computer" },
+            { word: "Kaffee", hint: "Ein beliebtes heißes Getränk" },
+            { word: "Schule", hint: "Ein Ort des Lernens" },
+            { word: "Park", hint: "Ein öffentlicher Garten oder Spielplatz" }
+        ],
+    },
+    {
+        name: "Level 3",
+        words: [
+            { word: "Elefant", hint: "Ein großes Tier mit einem Rüssel" },
+            { word: "Berg", hint: "Eine große Erhebung aus der Erde" },
+            { word: "Architektur", hint: "Die Kunst des Gebäudedesigns" },
+            { word: "Krokodil", hint: "Ein großes Reptil mit scharfen Zähnen" },
+            { word: "Universität", hint: "Ein Ort des höheren Lernens" },
+            { word: "Konservatorium", hint: "Eine Institution für musikalische Ausbildung" },
+            { word: "Astronaut", hint: "Eine Person, die ins Weltall fliegt" },
+            { word: "Parfüm", hint: "Ein duftendes Flüssigkeitsprodukt" },
+            { word: "Sushi", hint: "Ein japanisches Gericht mit rohem Fisch" },
+            { word: "Cappuccino", hint: "Ein Kaffeegetränk mit Milchschaum" },
+            { word: "Rendezvous", hint: "Ein geplantes Treffen zwischen zwei Personen" },
+            { word: "Croissant", hint: "Ein französisches Gebäck aus Blätterteig" }
+        ],
+    },
+];
