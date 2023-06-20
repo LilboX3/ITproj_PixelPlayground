229f7de8b627de3dc486746cf1e2ce68a9900d0e
ratat0uille <120578514+ratat0uille@users.noreply.github.com>
2023-06-20 15:44:06 +0200

    Update Snake.php

--- Snake.php
+++ Snake.php
@@ -5,6 +5,7 @@ include_once 'navbar.php';
     <title>Snake</title>
     <link rel="stylesheet" href="Snake/style.css">
     <link rel="stylesheet" href="Snake/prstartk.ttf">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 </head>
 <body class="background">
@@ -16,16 +17,21 @@ include_once 'navbar.php';
         </div>
     <?php }?>
 
-        <div class="gameField">
+        <div class="gameField" id="mode">
+            <div style="padding-top:10%; padding-bottom:20%; text-align: center">
+            <h1>Choose a game mode:</h1>
+            <br><br>
+            <button class="ezBtn" id="easy">Easy</button>
+            <button class="normBtn" id="normal">Normal</button>
+            <button class="hardBtn" id="hard">Hard</button>
+            </div>
+        </div>
+
+        <div class="gameField" id="gameField" style="display:none">
         <h1>SNAKE</h1>
         <div id="game">
             <canvas id="board" width="500" height="500"></canvas>
             <div id="scoreTxt">0</div>
-            <!--
-                maybe besser ein start game button
-                statt dass es automatisch anfängt lol
-                <button id="start">Start Game</button>
-            -->
             <button class="rst" id="reset">RESET</button>
         </div>
         </div>
@@ -33,5 +39,6 @@ include_once 'navbar.php';
     
 
     <script src="Snake/script.js"></script>
+    
 </body>
 </html>
