4a1ee5e4946791debfcdf6142b3525fbe955298c
Toukage <127383796+Toukage@users.noreply.github.com>
2023-06-21 21:01:19 +0200

    Update WordScramble.php

--- WordScramble.php
+++ WordScramble.php
@@ -8,36 +8,53 @@ include_once 'navbar.php';
 </head>
 
 <body class="background">
-    <div class="container">
-    <?php if(!isset($_SESSION['username'])){?>
-        <div class="entername" id="scramblebox">
-            <div style="font-size:small;">Enter your name to save your score !</div>
-            <input type="text" id="scramblename" placeholder="be creative!"> <button id="savescramble">Save</button>
+    <div class="parent-flex-container">
+        <div id="scoreboard" class="scoreboard">
+            <div class="score-label">SCORE:....<span id="score">0</span></div>
         </div>
-        <?php }?>
-
-        <header>
-            <h2>Word Scramble Game</h2>
-            <h5 class="num">Number of plays: <span></span></h5>
-            <h5 class="score">Score: <span></span></h5>
-        </header>
-
-        <div class="content">
-            <div class="word"></div>
-
-            <div class="detail">
-                <p class="hint">Hint: <span></span></p>
-                <p class="time">Time Left: <span><b></b>s</span></p>
+        <div id="topfive" class="topfive">
+            <span id="top5score"></span>
+        </div>
+        <div class="container">
+        <?php 
+                if (isset($_SESSION['username'])){ ?>
+                    <script>
+                        var username = '<?php echo $_SESSION['username']; ?>';
+                        console.log("Current username inside .php : ", username);
+                    </script>
+                    <?php 
+                } 
+        ?>
+        <?php if(!isset($_SESSION['username'])){?>
+            <div class="entername" id="scramblebox">
+                <div style="font-size:small;">Enter your name to save your score !</div>
+                <input type="text" id="scramblename" placeholder="be creative!"> <button id="savescramble">Save</button>
             </div>
-
-            <input type="text" class="placeHolder" id="input" placeholder="Enter a valid word">
-
-            <div class="btn">
-                <button class="refresh-word">Refresh Word</button>
-                <button class="check-word">Check Word</button>
-                <button class="levelbtn" data-level="0">Level 1</button>
-                <button class="levelbtn" data-level="1">Level 2</button>
-                <button class="levelbtn" data-level="2">Level 3</button>
+            <?php }?>
+    
+            <header>
+                <h2>Word Scramble Game</h2>
+                <h5 class="num">Number of plays: <span></span></h5>
+                <h5 class="score">Score: <span></span></h5>
+            </header>
+    
+            <div class="content">
+                <div class="word"></div>
+    
+                <div class="detail">
+                    <p class="hint">Hint: <span></span></p>
+                    <p class="time">Time Left: <span><b></b>s</span></p>
+                </div>
+    
+                <input type="text" class="placeHolder" id="input" placeholder="Enter a valid word">
+    
+                <div class="btn">
+                    <button class="refresh-word">Refresh Word</button>
+                    <button class="check-word">Check Word</button>
+                    <button class="levelbtn" data-level="0">Level 1</button>
+                    <button class="levelbtn" data-level="1">Level 2</button>
+                    <button class="levelbtn" data-level="2">Level 3</button>
+                </div>
             </div>
         </div>
     </div>
