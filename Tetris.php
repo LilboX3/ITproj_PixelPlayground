28b9427c98679c1fde37ba9dbcfa15709366f99d
Toukage <127383796+Toukage@users.noreply.github.com>
2023-06-21 21:35:04 +0200

    Update Tetris.php

--- Tetris.php
+++ Tetris.php
@@ -7,28 +7,45 @@ include_once 'navbar.php';
     <link rel="stylesheet" href="Tetris/prstartk.ttf">
 </head>
 <body class="background">
-    <div class="container ">
-        <div class="row">
-            <div class="col-md-12 d-flex justify-content-center align-items-center">
-            <?php if(!isset($_SESSION['username'])){?>
-            <div class="entername" id="tetrisbox">
-                        <div style="font-size:small;">Enter your name to save your score !</div>
-                        <input type="text" id="tetrisname" placeholder="be creative!"> 
-                        <button id="savetetris">Save</button>
+    <div class="parent-flex-container">
+        <div id="scoreboard" class="scoreboard">
+            <div class="score-label">SCORE:....<span id="score">0</span></div>
+        </div>
+        <div id="topfive" class="topfive">
+            <span id="top5score"></span>
+        </div>
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-12 d-flex justify-content-center align-items-center">
+                <?php 
+                    if (isset($_SESSION['username'])){ ?>
+                        <script>
+                            var username = '<?php echo $_SESSION['username']; ?>';
+                            console.log("Current username inside .php : ", username);
+                        </script>
+                        <?php 
+                    } 
+                ?>
+                <?php if(!isset($_SESSION['username'])){?>
+                <div class="entername" id="tetrisbox">
+                            <div style="font-size:small;">Enter your name to save your score !</div>
+                            <input type="text" id="tetrisname" placeholder="be creative!"> 
+                            <button id="savetetris">Save</button>
+                </div>
+            </div>
+            <?php }?>
+            </div>
+    
+                <div class="gameField d-flex justify-content-center align-items-center">
+                    <!--<div id="logo-container">
+                    <img src="Tetris/pics/tetrislogo.png" alt="Tetris Logo" />
+                    </div>-->
+                    <div class="mt-5 mb-5">
+                    <canvas id="tetris"></canvas>
+                    </div>
+                </div><
             </div>
         </div>
-        <?php }?>
-        </div>
-
-            <div class="gameField d-flex justify-content-center align-items-center">
-                <!--<div id="logo-container">
-                <img src="Tetris/pics/tetrislogo.png" alt="Tetris Logo" />
-                </div>-->
-                <div class="mt-5 mb-5">
-                <canvas id="tetris"></canvas>
-                </div>
-            </div><
-    </div>
             
 
 
